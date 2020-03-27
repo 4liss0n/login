12c78138c3d5cf351f1033f80865578536a280e6
4liss0n <61910302+4liss0n@users.noreply.github.com>
2020-03-27 13:44:28 -0300

    Update exercicio2.py

--- exercicio2.py
+++ exercicio2.py
@@ -1,14 +1,15 @@
-nivel = input(print("bom dia, o que você é? Adm(a), User(a) ou Visitante")).upper()
-
-if nivel == "ADM":
+nome = input("qual o seu nome? ")
+nivel = input(print("bom dia, o que você é? Adm, User ou Visitante")).upper()
+genero = input(print("qual o seu nome?(M/F) ")).upper()
+if nivel == "ADM" and genero == "M":
     print("bem-vindo administrador, por favor ensira seu e-mail e senha")
-elif nivel == "ADMA":
+elif nivel == "ADM" and genero =="F":
     print("bem-vindo administradora, por favor ensira seu e-mail e senha")
-elif nivel == "USER":
+elif nivel == "USER" and genero == "M":
     print("bem-vindo usuario, por favor ensira seu nome e senha")
-elif nivel == "USERA":
+elif nivel == "USERA"and genero == "F":
     print("bem-vindo usuaria, por favor ensira seu nome e senha")
 elif nivel == "VISITANTE":
     print("bem-vindo visitante, alguns dos seus acessos foram limitados")
 else:
-    print("sai daqui estranho(a)")
+    print("bem-vindo estranho")
